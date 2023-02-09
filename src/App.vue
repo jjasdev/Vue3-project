1a339bed75213855a6d006d70864be1fc5826cae
juanjoalsz <juanjoalsz@gmail.com>
2023-02-09 23:47:35 +0100

    Inicio del proyecto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hola</h1>
+    <p>mundo</p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
